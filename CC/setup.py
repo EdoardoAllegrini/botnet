96c737a6b349fac37c8331b4e525b064f08d68b2
EdoardoAllegrini <edoardoallegrini01@gmail.com>
2023-05-26 11:42:58 +0200

    Removed config.py sens

---
+++ CC/setup.py
@@ -0,0 +1,19 @@
+setup(
+    name="CC of a botnet",
+    version="1.0.0",
+    description="Botnet theatre",
+    long_description=README,
+    long_description_content_type="text/markdown",
+    url="https://github.com/EdoardoAllegrini/botnet",
+    author="Edoardo Allegrini",
+    license="MIT",
+    classifiers=[
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 2",
+        "Programming Language :: Python :: 3",
+    ],
+    packages=["CC"],
+    include_package_data=True,
+    install_requires=[]
+)
