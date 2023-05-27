d7436b90b2ecdeafa2a8bd81e4ba2b913e0e6768
EdoardoAllegrini <edoardoallegrini01@gmail.com>
2023-05-27 17:20:58 +0200

    Removes old dependencies files

--- CC/cli.py
+++
@@ -1,4 +0,0 @@
-from main import server_program
-
-if __name__ == '__main__':
-    server_program()
--- bot/cli.py
+++
@@ -1,4 +0,0 @@
-from main import client_program
-
-if __name__ == '__main__':
-    client_program()
