52c2e604a0bac52d428d42825359917e5d727ea2
EdoardoAllegrini <allegrini.1969146@studenti.uniroma1.it>
2023-06-27 15:42:46 +0200

    reAdd send_email.py

---
+++ bot/send_email.py
@@ -0,0 +1,62 @@
+import smtplib, ssl
+import sys
+from email.mime.text import MIMEText
+from email.mime.multipart import MIMEMultipart
+
+sys.path.append('<path botnet root dir>')
+import config
+
+
+def send_email(receiver_email, subject, plain_text, html):
+    sender_email = "<your_email_domain>"
+    password = config.email_password
+
+    message = MIMEMultipart("alternative")
+    message["Subject"] = subject
+    message["From"] = sender_email
+    message["To"] = receiver_email
+
+    # Create the plain-text and HTML version of your message
+    # text = """\
+    # Hi,
+    # How are you?
+    # Real Python has many great tutorials:
+    # www.realpython.com"""
+    # html = """\
+    # <html>
+    # <body>
+    #     <p>Hi,<br>
+    #     How are you?<br>
+    #     <a href="http://www.realpython.com">Real Python</a> 
+    #     has many great tutorials.
+    #     </p>
+    # </body>
+    # </html>
+    # """
+
+    # Turn these into plain/html MIMEText objects
+    part1 = MIMEText(plain_text, "plain")
+    part2 = MIMEText(html, "html")
+
+    # Add HTML/plain-text parts to MIMEMultipart message
+    # The email client will try to render the last part first
+    message.attach(part1)
+    message.attach(part2)
+
+    # Create secure connection with server and send email
+    context = ssl.create_default_context()
+    with smtplib.SMTP_SSL("smtp.gmail.com", 465, context=context) as server:
+        server.login(sender_email, password)
+        server.sendmail(
+            sender_email, receiver_email, message.as_string()
+        )
+    return 1
+
+
+
+if __name__ == "__main__":
+    import json
+    with open(r"/botnet/CC/email.json") as f:
+        f_d = json.load(f)
+
+    send_email("<receiver>", f_d["subject"], f_d["plaintext"], f_d["html"])
